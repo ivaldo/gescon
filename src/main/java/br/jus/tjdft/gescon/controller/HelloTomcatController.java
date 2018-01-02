19368853b031f46ccd073d8014ed9bea29c1f307
Mona Lisa <ivaldo.oliveira@gmail.com>
2018-01-01 23:11:23 -0200

    inclusao da dependencia spring-rest-data

--- src/main/java/br/jus/tjdft/gescon/controller/HelloTomcatController.java
+++ src/main/java/br/jus/tjdft/gescon/controller/HelloTomcatController.java
@@ -1,14 +1,14 @@
-package bootwildfly;
+package br.jus.tjdft.gescon.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWildFlyController {
-
+public class HelloTomcatController {
 
     @RequestMapping("hello")
     public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+        return "Hello, SpringBoot on Tomcat";
     }
+    
 }
